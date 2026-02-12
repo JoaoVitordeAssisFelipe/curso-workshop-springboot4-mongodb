317a4a3a7ae78d02f63110494c00e36ae7d45a97
JoaoVitordeAssisFelipe <jaovitordeassis@gmail.com>
2026-02-11 22:47:44 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/joaovitor/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.joaovitor.workshopmongo.config;
+
+import com.joaovitor.workshopmongo.Repository.UserRepository;
+import com.joaovitor.workshopmongo.domain.User;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public Instantiation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null,"Maria Brown","maria@gmail.com");
+        User alex = new User(null,"Alex Green","Alex@gmail.com");
+        User bob = new User(null,"Bob Grey","bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+    }
+}
